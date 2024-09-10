f34b29b4c6f771fe522abc55f285ecda69140b4a
ali <ama6708@g.rit.edu>
2024-09-10 20:47:57 +0400

    Variables Activity

---
+++ variables1.py
@@ -0,0 +1,15 @@
+def variables_practice():
+    age = 17
+    months = 12
+    print("My age in months is: ", age * months)
+
+    days = 365
+    year = 1
+    print("The number of days in a year is: ", days * year)
+    
+    pet = "ali"
+    print("The number of my first pet is: ", pet)
+
+    pi = 3.14159
+    print("The first 5 digits of Pi is: ", pi)
+variables_practice()
